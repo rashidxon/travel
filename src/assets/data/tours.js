87fff2410c1afc2e4aab690b08a28d760fcd15d4
NuriddinovRashidxon <118523618+rashidxon@users.noreply.github.com>
2024-06-10 00:40:26 +0500

    Update tours.js

--- src/assets/data/tours.js
+++ src/assets/data/tours.js
@@ -10,13 +10,13 @@ import tourImg08 from "../images/tour-img08.jpg";
 const tours = [
   {
     id: "01",
-    title: "Qo'qon",
+    title: "Xudoyorxon oʻrdasi, Qo'qon",
     city: "Uzbekistan",
     distance: 300,
     address: "Farg'ona",
     price: 80,
     maxGroupSize: 10,
-    desc: "this is the description",
+    desc: "Xudoyorxon oʻrdasi — Arki oliy Sayyid Muhammad Xudoyorxon (rasmiy nomi). Meʼmoriy yodgorlik (XX asrning ikkinchi yarmida); Qoʻqon xoni Xudoyorxon qurdirgan. Meʼmor va muhandis Mir Ubaydulla Muhandis loyihasi va rahbarligida bunyod etilgan; ustalardan Mulla Suyarqul, Solixoʻja va buxorolik usta Fozilxoʻjalar qatnashgan, koshinlarini rishtonlik kulol usta Abdulla mahorat bilan bajargan.Xon qarorgohi, oʻrda ahli yashaydigan hamda turli maqsadlarda foydalaniladigan 100 dan ortiq katta-kichik xonalar, ichki-tashqi hovlilardan iborat boʻlgan. Birinchi hovlida ayvon, koʻrinishxona (salomxona), xazinaxona, alohida saxdli masjid, Keyingi hovli xon qarorgohi (shohnishin), xoʻjalik binolari, binoning anfi qismi haram va xos xonalar egallagan; haramxonalari bezaklari odmi boʻlib, asosan, quyma ganch.",
     reviews: [
       {
         name: "Foziljon",
@@ -39,7 +39,7 @@ const tours = [
     address: "Buxoro",
     price: 50,
     maxGroupSize: 8,
-    desc: "Sayohat ajoyib o'tdi raxmat",
+    desc: "Mir Arabning asl ismi Sayyid Abdulloh boʻlib, amir 22 yoshida Samarqandga kelib Xoja Ahrorga shogird tushgan. Turkistonning Sabronida (Savronida) 2 koriz (yer osti arigʻi) qazdirgan, suv chiqargan, qalʼa bino etib, Shofirkon, Vobkent, Gʻijduvonda koʻp joylarni obod qilgan (hozirda Shofirkonda Mir Arab qalʼasi saqlanib qolgan).",
     reviews: [
       {
         name: "Shoxruh",
@@ -58,7 +58,7 @@ const tours = [
     address: "Samarqand",
     price: 65,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "Qadimiy Samarqand shahrining rasmiy markazi Registon maydoni boʻlib, bu yerda uchta madrasa qad koʻtargan: Ulugʻbek, Sherdor va Tillakori madrasalari. Registon – qadimiy ilm, taʼlim muassasalari joylashgan joy boʻlib, sharqdagi shahar qurilishi sanʼatining eng koʻzga koʻrinarli namunalaridan biri hisoblanadi. U haqda Temuriylar faxr bilan: „Kim bizning kuch-qudratimizga shubha qilsa, kelib biz qurgan binolarni koʻrsin“, deganlar. 2001-yilda bu uch madrasa UNESCOning butun dunyo yodgorliklari roʻyxatiga kiritilgan.	",
     reviews: [
       {
         name: "Otabek",
@@ -71,13 +71,13 @@ const tours = [
   },
   {
     id: "04",
-    title: "Ichan qala, Xorazm",
+    title: "Ichan qal’a, Xorazm",
     city: "Uzbekistan",
     distance: 500,
     address: "Xorazm",
     price: 60,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "Ichan-qal’a (oʻzbekcha: Ichan Qa'la) – Oʻzbekistonning tarixiy oʻzbek shahri Xivaning mustahkam devor bilan oʻralgan ichki shahri boʻlib, Markaziy Osiyodagi birinchi Jahon meʼrosi ob’ektiga aylandi. Asrlar davomida qad rostlagan (eng qadimiy meʼmoriy yodgorliklar XIV asrga toʻgʻri keladi), istehkom, saroylar, masjid, madrasalar, maqbara, minoralar, karvonsaroy va hammomlar Ichan qalʼani oʻziga xos yodgorlik shahriga aylantirdi. Koʻhna Xiva 1968-yilda rasman qoʻriqxona maqomini oldi.",
     reviews: [
       {
         name: "Shukrullo",
@@ -90,13 +90,13 @@ const tours = [
   },
   {
     id: "05",
-    title: "Toshkent",
+    title: "Amir Temur xiyoboni, Toshkent",
     city: "Uzbekistan",
     distance: 500,
     address: "Somewhere",
     price: 65,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "Amir Temur xiyoboni - Toshkent metropoliteni Chilonzor yoʻlining bekatlaridan biri. «Mustaqillik maydoni» va «Hamid Olimjon» bekatlari oʻrtasida joylashgan. Bekatdan Yunusobod yoʻlining Yunus Rajabiy bekatiga oʻtiladi. Amir Temur xiyoboni sharafiga nomlangan. Bekat kolonnali, 2 vestibyul va 1 orolchali platformadan iborat.",
     reviews: [
       {
         name: "Bobur",
@@ -115,7 +115,7 @@ const tours = [
     address: "Somewhere",
     price: 110,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "1930-yillarda Oʻzbekiston va Qirgʻiziston oʻrtasidagi hududlar almashinuvi natijasida Shoximardon Oʻzbekiston tarkibiga kirib, anklav — Qirgʻizistonning Botken viloyati yerlari bilan oʻralgan oʻziga xos kichik orolni tashkil qildi; Oʻzbekistonning “asosiy” hududigacha boʻlgan masofa taxminan 17 km.",
     reviews: [
       {
         name: "Azizbek",
@@ -128,13 +128,13 @@ const tours = [
   },
   {
     id: "07",
-    title: "Jomin, Jizzax",
+    title: "Zomin, Jizzax",
     city: "Uzbekistan",
     distance: 250,
     address: "Jizzax",
     price: 70,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "Zomin-O‘zbek Shveytsariyasi.Ajoyib landshaftlar va toza tog‘ havosi, flora va faunaning  xilma-xilligi, qadimiy ziyoratgohlar va noyob milliy sihatgoh - bularning barchasi Zomin.",
     reviews: [
       {
         name: "Jamshid",
@@ -153,7 +153,7 @@ const tours = [
     address: "Samarqand",
     price: 99,
     maxGroupSize: 8,
-    desc: "this is the description",
+    desc: "Shohizinda meʼmoriy majmuasi Qoraxoniylar sulolasi tomonidan asos solingan.Ularning eng qadimiysi Qusam ibn Abbos maqbarasi boʻlib, xalq orasida Shohizinda (tirik shoh) nomi bilan mashhur. Ansambl bir-biri bilan yoʻlak orqali bogʻlangan 3 guruh binolardan iborat. Quyi guruhdagi inshootlar Ulugʻbek oʻgʻli nomidan qurdirgan Abdulaziz chortogʻi (1434—35), unga shim.dan Davlat qushbegi madrasasi (1812—13) tutash, uning qarshisidagi ayvonli masjid naqshlar bilan nafis bezatilgan. 40 bosqichli tik zinapoya oʻrtaligʻining chap tomonida ziyoratxona va goʻrxonadan iborat qoʻshgumbazli maqbara (15-asr) joylashgan.",
     reviews: [
       {
         name: "Abduqodir",
